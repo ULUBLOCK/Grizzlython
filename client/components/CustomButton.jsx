cbb7f073836f8688f70225577d107634ef730ffb
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-09 16:12:49 +0300

    commit

--- client/components/CustomButton.jsx
+++ client/components/CustomButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const CustomButton = ({ btnType, title, handleClick, styles }) => {
+const CustomButton = ({ btnType, title, handleClick }) => {
   return (
     <button
       type={btnType}
-      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
+      className="btn glow-on-hover"
       onClick={handleClick}
     >
       {title}
--- client/pages/createEvent.tsx
+++ client/pages/createEvent.tsx
@@ -28,11 +28,9 @@ const CreateEvent = () => {
 
 
   return (
-    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+    <div className="mx-10 flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
       {isLoading && <Loader />}
-      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
-        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start a Campaign</h1>
-      </div>
+        <h1 className="">Start a Community  Event</h1>
 
       <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
         <div className="flex flex-wrap gap-[40px]">
@@ -95,10 +93,10 @@ const CreateEvent = () => {
           <div className="flex justify-center items-center mt-[40px]">
             <CustomButton 
             btnType="submit"
-            title="Submit new campaign"
-            styles="bg-[#6699CC]" 
+            title="Submit new event"
             handleClick={undefined}            
           />
+
           </div>
       </form>
     </div>

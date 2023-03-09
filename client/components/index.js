02f4d210607536c16f71f7f0c120e2fc7531f63e
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-09 16:31:54 +0300

    commit

--- client/components/index.js
+++ client/components/index.js
@@ -1,3 +1,5 @@
 export {default as CustomButton} from './CustomButton';
 export {default as Loader} from './Loader';
-export {default as FormField} from './FormField';
+export { default as FormField } from './FormField';
+export { default as DisplayEvents } from './DisplayEvents';
+export { default as FundCard } from './FundCard';
--- client/pages/communities.tsx
+++ client/pages/communities.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { DisplayEvents } from '../components';
+import { useStateContext } from '../context/context'
 
 const communities = () => {
-  return (
-    <div>
-      all communities
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
 
-      create community button
+  //const { address, contract, getCampaigns } = useStateContext();
+
+  const address="xxxxxxxxx"
+  const contract="xxxxxxxxx"
+  
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+   // const data = await getCampaigns();
+   // setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  /*useEffect(() => {
+    if(contract) fetchCampaigns();
+  }, [address, contract]);
+*/
+  return (
+    <div className=''>
+    <h1 className='text-3xl text-center'>All Community Events</h1>
+    <DisplayEvents 
+      title="All Community events"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
     </div>
   )
 }
